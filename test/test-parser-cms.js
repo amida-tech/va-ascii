514a421a74b6b03081af656968f5c32beaffd356
OEHokie <matt.martz@gmail.com>
2015-08-11 12:17:23 -0400

    object to array

--- test/test-parser-cms.js
+++ test/test-parser-cms.js
@@ -9,25 +9,16 @@ var split = require('split');
 var bbva = require("../index");
 
 describe('VA ASCII to Intermediate Object', function () {
+    var outputObj;
     it('should convert to Int Obj', function (done) {
         var istream = fs.createReadStream(__dirname + '/fixtures/va-ascii.txt', 'ascii');
 
         expect(istream).to.exist;
 
-        //convert string into JSON
-        //var result = bbcms.parseText(txtfile);
-        /*var result = bbcms.test(istream.pipe(split()), function(result) {
-            expect(result).to.exist;
-            console.log(result[0]);
-            done();
-        }, function(e) { 
-            console.log(e);
-            done(e);
-        } );*/
-
         istream.pipe(split()).pipe(new bbva.VaAsciiFile2Object())
             .on('data', function (data) {
-                console.log(JSON.stringify(data, null, '    '));
+                expect(data).to.not.be.empty;
+                outputObj = data;
             })
             .on('finish', function () {
                 done();
@@ -35,33 +26,18 @@ describe('VA ASCII to Intermediate Object', function () {
             .on('error', function (error) {
                 done(error);
             });
-
-        /*var valid = bbm.validator.validateDocumentModel(result);
-
-        if (!valid) {
-            console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-        }
-
-        expect(valid).to.be.true;
-        */
-
     });
+    it('should have the right sections', function (done) {
+        expect(outputObj).to.contain.all.keys(['header', 'request_summary', 'account_summary', 'self_demographics', 'va_demographics', 'self_providers', 'self_facilities', 'self_insurance', 'va_reminders', 'va_appointments', 'va_allergies', 'self_allergies', 'va_medications', 'self_medications', 'va_problems', 'va_admission_discharge', 'va_notes', 'self_events', 'va_immunizations', 'self_immunizations', 'va_labs', 'va_pathology', 'self_labs', 'va_vitals', 'self_vitals', 'va_radiology', 'va_ekg', 'self_family', 'self_military', 'self_activity', 'self_food', 'dod_service', 'self_current_goals', 'self_completed_goals']);
+        done();
+    });
+});
 
+describe('Intermediate Object to FHIR', function () {
     it('CMS parser/converter test', function (done) {
         var istream = fs.createReadStream(__dirname + '/fixtures/va-ascii.txt', 'ascii');
 
         expect(istream).to.exist;
-
-        //convert string into JSON
-        //var result = bbcms.parseText(txtfile);
-        /*var result = bbcms.test(istream.pipe(split()), function(result) {
-            expect(result).to.exist;
-            console.log(result[0]);
-            done();
-        }, function(e) { 
-            console.log(e);
-            done(e);
-        } );*/
 
         istream.pipe(split())
             .pipe(new bbva.VaAsciiFile2Object())
@@ -75,36 +51,5 @@ describe('VA ASCII to Intermediate Object', function () {
             .on('error', function (error) {
                 done(error);
             });
-
-        /*var valid = bbm.validator.validateDocumentModel(result);
-
-        if (!valid) {
-            console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-        }
-
-        expect(valid).to.be.true;
-        */
-
     });
-
-    /*    it('Extra line breaks before sections', function (done) {
-            var txtfile = fs.readFileSync(__dirname + '/fixtures/sample-extra.txt', 'utf-8');
-
-            expect(txtfile).to.exist;
-
-            //convert string into JSON
-            var result = bbcms.parseText(txtfile);
-            expect(result).to.exist;
-
-            var valid = bbm.validator.validateDocumentModel(result);
-
-            if (!valid) {
-                console.log("Errors: \n", JSON.stringify(bbm.validator.getLastError(), null, 4));
-            }
-
-            expect(valid).to.be.true;
-
-            done();
-        });
-    */
 });

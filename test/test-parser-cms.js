ad241257c5e5ed4b17ab4e0c828e1b267001ddfe
OEHokie <matt.martz@gmail.com>
2015-08-11 15:39:22 -0400

    fixed immunization parsing error

--- test/test-parser-cms.js
+++ test/test-parser-cms.js
@@ -51,9 +51,23 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.self_demographics).to.contain.all.keys(["source", "first_name", "middle_initial", "last_name", "suffix", "alias", "relationship_to_va", "gender", "bloodtype", "organdonor", "date_of_birth", "marital_status", "current_occupation", "mailing_or_destination_address", "mailing_or_destination_address2", "mailing_or_destination_city", "mailing_or_destination_state", "mailing_or_destination_country", "mailing_or_destination_province", "mailing_or_destination_zip/postal_code", "home_phone_number", "work_phone_number", "pager_number", "cell_phone_number", "fax_number", "email_address", "preferred_method_of_contact", "emergencycontacts"]);
         done();
     });
+    it('self_demographics should have two emergency contacts', function (done) {
+        expect(outputObj.self_demographics.emergencycontacts).to.exist;
+        expect(outputObj.self_demographics.emergencycontacts).to.have.length(2);
+        expect(outputObj.self_demographics.emergencycontacts[1].contact_first_name).to.exist;
+        expect(outputObj.self_demographics.emergencycontacts[1].contact_first_name).to.equal("Three");
+        done();
+    });
     it('va_demographics should have the right keys', function (done) {
         expect(outputObj.va_demographics).to.exist;
         expect(outputObj.va_demographics).to.contain.all.keys(["source", "last_updated", "sorted_by", "facilities"]);
+        done();
+    });
+    it('va_demographics should have three facilities', function (done) {
+        expect(outputObj.va_demographics.facilities).to.exist;
+        expect(outputObj.va_demographics.facilities).to.have.length(3);
+        expect(outputObj.va_demographics.facilities[0].va_treating_facility).to.exist;
+        expect(outputObj.va_demographics.facilities[0].va_treating_facility).to.equal("VA SOUTHERN OREGON REHABILITATION CENTER CLINICS");
         done();
     });
     it('self_providers should have the right keys', function (done) {
@@ -81,9 +95,23 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.va_appointments).to.contain.all.keys(["source", "last_updated", "sort_by", "pastappointments"]);
         done();
     });
+    it('va_appointments should have the right history length', function (done) {
+        expect(outputObj.va_appointments.pastappointments).to.exist;
+        expect(outputObj.va_appointments.pastappointments).to.have.length(8);
+        expect(outputObj.va_appointments.pastappointments[0].clinic).to.exist;
+        expect(outputObj.va_appointments.pastappointments[0].clinic).to.equal("GEN SURG HERNIA-4A103");
+        done();
+    });
     it('va_allergies should have the right keys', function (done) {
         expect(outputObj.va_allergies).to.exist;
         expect(outputObj.va_allergies).to.contain.all.keys(["source", "last_updated", "allergies"]);
+        done();
+    });
+    it('va_allergies should have the right length', function (done) {
+        expect(outputObj.va_allergies.allergies).to.exist;
+        expect(outputObj.va_allergies.allergies).to.have.length(6);
+        expect(outputObj.va_allergies.allergies[2].comments).to.exist;
+        expect(outputObj.va_allergies.allergies[2].comments).to.contain("might"); //tests that the newline comments were received/stored
         done();
     });
     it('self_allergies should have the right keys', function (done) {
@@ -91,9 +119,21 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.self_allergies).to.contain.all.keys(["source", "allergies"]);
         done();
     });
+    it('self_allergies should have the right length', function (done) {
+        expect(outputObj.self_allergies.allergies).to.exist;
+        expect(outputObj.self_allergies.allergies).to.have.length(2);
+        done();
+    });
     it('va_medications should have the right keys', function (done) {
         expect(outputObj.va_medications).to.exist;
         expect(outputObj.va_medications).to.contain.all.keys(["source", "last_updated", "sorted_by", "medications"]);
+        done();
+    });
+    it('va_medications should have the right length', function (done) {
+        expect(outputObj.va_medications.medications).to.exist;
+        expect(outputObj.va_medications.medications).to.have.length(32);
+        expect(outputObj.va_medications.medications[30].instructions).to.exist;
+        expect(outputObj.va_medications.medications[30].instructions).to.have.length(3);
         done();
     });
     it('self_medications should have the right keys', function (done) {
@@ -101,9 +141,19 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.self_medications).to.contain.all.keys(["source", "medications"]);
         done();
     });
+    it('self_medications should have the right length', function (done) {
+        expect(outputObj.self_medications.medications).to.exist;
+        expect(outputObj.self_medications.medications).to.have.length(2);
+        done();
+    });
     it('va_problems should have the right keys', function (done) {
         expect(outputObj.va_problems).to.exist;
         expect(outputObj.va_problems).to.contain.all.keys(["source", "last_updated", "sorted_by", "problems"]);
+        done();
+    });
+    it('va_problems should have the right length', function (done) {
+        expect(outputObj.va_problems.problems).to.exist;
+        expect(outputObj.va_problems.problems).to.have.length(5);
         done();
     });
     it('va_admission_discharge should have the right keys', function (done) {
@@ -111,9 +161,21 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.va_admission_discharge).to.contain.all.keys(["source", "last_updated", "sorted_by", "admindischarge"]);
         done();
     });
+    it('va_admission_discharge should have the right length', function (done) {
+        expect(outputObj.va_admission_discharge.admindischarge).to.exist;
+        expect(outputObj.va_admission_discharge.admindischarge).to.have.length(1);
+        expect(outputObj.va_admission_discharge.admindischarge[0].summary).to.exist;
+        expect(outputObj.va_admission_discharge.admindischarge[0].summary).to.have.length(44);
+        done();
+    });
     it('va_notes should have the right keys', function (done) {
         expect(outputObj.va_notes).to.exist;
         expect(outputObj.va_notes).to.contain.all.keys(["source", "last_updated", "sorted_by", "newnote", "notes"]);
+        done();
+    });
+    it('va_notes should have the right length', function (done) {
+        expect(outputObj.va_notes.notes).to.exist;
+        expect(outputObj.va_notes.notes).to.have.length(12);
         done();
     });
     it('self_events should have the right keys', function (done) {
@@ -126,14 +188,33 @@ describe('VA ASCII to Intermediate Object', function () {
         expect(outputObj.va_immunizations).to.contain.all.keys(["source", "last_updated", "sorted_by", "summary", "immunizations"]);
         done();
     });
+    it('va_immunizations should have the right length', function (done) {
+        expect(outputObj.va_immunizations.summary).to.exist;
+        expect(outputObj.va_immunizations.summary).to.have.length(5);
+        expect(outputObj.va_immunizations.immunizations).to.exist;
+        expect(outputObj.va_immunizations.immunizations).to.have.length(5);
+        done();
+    });
     it('self_immunizations should have the right keys', function (done) {
         expect(outputObj.self_immunizations).to.exist;
         expect(outputObj.self_immunizations).to.contain.all.keys(["source", "lastkey", "immunizations"]);
         done();
     });
+    it('self_immunizations should have the right length', function (done) {
+        expect(outputObj.self_immunizations.immunizations).to.exist;
+        expect(outputObj.self_immunizations.immunizations).to.have.length(2);
+        done();
+    });
     it('va_labs should have the right keys', function (done) {
         expect(outputObj.va_labs).to.exist;
-        expect(outputObj.va_labs).to.contain.all.keys(["source", "newtest", "newlab", "lastkey", "last_updated", "sorted_by", "labs"]);
+        expect(outputObj.va_labs).to.contain.all.keys(["source", "lastkey", "last_updated", "sorted_by", "labs"]);
+        done();
+    });
+    it('va_labs should have the right length', function (done) {
+        expect(outputObj.va_labs.labs).to.exist;
+        expect(outputObj.va_labs.labs).to.have.length(36);
+        expect(outputObj.va_labs.labs[3].tests).to.exist;
+        expect(outputObj.va_labs.labs[3].tests).to.have.length(6);
         done();
     });
     it('va_pathology should have the right keys', function (done) {
